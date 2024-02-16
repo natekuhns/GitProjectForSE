eb674e42d097d03917c7ff785b85e4c2eb203dd2
KUHNSNJ21 <n8kuhns@gmail.com>
2024-02-16 14:47:10 -0500

    3rd commit after renaming file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
-public class Glob {
+public class Main {
     public static void main(String[] args) {
         System.out.println("globster");
         System.out.println("second line");
+        //commit -m : add message to commit
     }
 }
