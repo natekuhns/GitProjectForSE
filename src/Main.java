e3ca0bc59d9e9cb6399ad0bbcee973e32ae56125
KUHNSNJ21 <n8kuhns@gmail.com>
2024-02-19 14:09:10 -0500

    Adding a 3rd line to main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("globster");
         System.out.println("second line");
+        System.out.println("3rd line");
         //commit -m : add message to commit
     }
 }
