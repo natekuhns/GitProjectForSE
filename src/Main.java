f7a26d6c0abb1a9f050ca97233493fbd7734e39b
KUHNSNJ21 <n8kuhns@gmail.com>
2024-02-19 14:49:50 -0500

    other commit to second branch

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("3rd line");
         System.out.println();
         System.out.println("Sigmund says hi");
+        System.out.println("new commit to other branch");
         //commit -m : add message to commit
 
     }
